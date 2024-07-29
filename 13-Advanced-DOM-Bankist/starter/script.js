4e83720ebbbb9efe400ee95cfb0fea7d84e27af5
bornsrss14 <bornsrss14@gmail.com>
2024-07-28 23:25:39 -0600

    Feature: I just added event listeners to the dots so that each dot functions to slide between testimonials.

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -12,6 +12,8 @@ const slides = document.querySelectorAll('.slide');
 const sliderEntero = document.querySelector('.slider');
 const btnLeft = document.querySelector('.slider__btn--left');
 const btnRight = document.querySelector('.slider__btn--right');
+const dotsContainer = document.querySelector('.dots');
+const dots = document.querySelectorAll('.dots__dot');
 
 /* FUNCIONES GENERALES */
 const openModal = function () {
@@ -33,19 +35,44 @@ document.addEventListener('keydown', function (e) {
 });
 
 const funSmoothScroll = function (e) {};
+const funCreateDots = function () {
+  slides.forEach(function (_, index) {
+    dotsContainer.insertAdjacentHTML(
+      'beforeend',
+      `<button class="dots__dot" data-slide="${index}"></button>`
+    );
+  });
+};
+funCreateDots();
+const activateDot = function (slide) {
+  document
+    .querySelectorAll('.dots__dot')
+    .forEach(dot => dot.classList.remove('dots__dot--active'));
+
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add('dots__dot--active');
+};
+activateDot(0);
+
+const funDotClick = function (e) {
+  if (e.target.classList.contains('dots__dot')) {
+    const { slide } = e.target.dataset;
+    goToSlide(slide);
+    activateDot(slide);
+  }
+};
 
 /*F. Slider */
 
 let currentSlide = 0;
 const maxSlides = slides.length;
+
 const goToSlide = function (slide) {
   slides.forEach(
     (s, index) => (s.style.transform = `translateX(${100 * (index - slide)}%)`)
   );
 };
-slides.forEach(
-  (s, index) => (s.style.transform = `translateX(${100 * index}%)`)
-);
 goToSlide(0);
 
 const funNextSlide = function () {
@@ -55,11 +82,13 @@ const funNextSlide = function () {
     currentSlide++;
   }
   goToSlide(currentSlide);
+  activateDot(currentSlide);
 }; /* currentSlide = 1:  -100 , 0 , 100 , 200 , and do far and so on */
 
 const funPreviousSlide = function () {
   currentSlide === 0 ? (currentSlide = maxSlides - 1) : currentSlide--;
   goToSlide(currentSlide);
+  activateDot(currentSlide);
 };
 
 /* EVENT LISTENERS BUTTONS */
@@ -68,3 +97,12 @@ overlay.addEventListener('click', closeModal);
 btn_scrollTo.addEventListener('click', funSmoothScroll);
 btnRight.addEventListener('click', funNextSlide);
 btnLeft.addEventListener('click', funPreviousSlide);
+dotsContainer.addEventListener('click', funDotClick);
+document.addEventListener('keydown', function (ev) {
+  if (ev.key === 'ArrowLeft') {
+    funPreviousSlide();
+  }
+  if (ev.key === 'ArrowRight') {
+    funNextSlide();
+  }
+});
